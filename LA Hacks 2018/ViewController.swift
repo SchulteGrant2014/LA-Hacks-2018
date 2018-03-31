3968b6005d2759e3bb4e53bf3b213ba406c6b9a9
Shirlyn Tang <shirlyntang@gmail.com>
2018-03-31 04:54:44 -0700

    basic frontend

--- LA Hacks 2018/ViewController.swift
+++ LA Hacks 2018/ViewController.swift
@@ -2,14 +2,26 @@
 //  ViewController.swift
 //  LA Hacks 2018
 //
-//  Created by Grant Schulte on 3/31/18.
+//  Created by Shirlyn Tang on 3/31/18.
 //  Copyright © 2018 Consonants. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate,
+    UINavigationControllerDelegate
+ {
 
+    @IBAction func openImageViewController(_ sender: Any) {
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.sourceType = .camera;
+            imagePicker.allowsEditing = false
+            self.present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
